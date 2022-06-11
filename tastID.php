3924f1c1479528a4da05f9d29134bad528d1198c
nihoex <nitiphongstudio@gmail.com>
2022-06-11 09:51:27 +0700

    updatefile tastId.php.

---
+++ tastID.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <body>
+        <from>
+            ชื่อ <input type="text"
+            name="firstname"
+            value="Earn"><br>
+            นามสกุล <input type="text"
+            name="lastname"><br>
+            รหัส <input type="password"
+            name="pwd"><br>
+            E-mail: <input type="email"
+            name="emailaddress"><br>
+            <input type="submit"
+            value="ตกลงสมัครสมาชิก">
+            <input type="reset"
+            value="ยกเลิก">
+        </from>
+    </body>
+</html>
